6cc252436746dae215eedf9c9f804088fac7668c
Cameron Woodmansee <cameron29w@gmail.com>
2017-09-14 22:25:06 -0400

    Add Meow (mongodb orm) and remove fluent
    Update schema query to correctly return values from mongo.
    Hopefully with Swift 4 some of the code generation will not be needed.

--- Package.swift
+++ Package.swift
@@ -4,21 +4,20 @@ let package = Package(
     name: "Vapor",
     targets: [
         Target(name: "App"),
-        Target(name: "Run", dependencies: ["App"]),
+        Target(name: "Run", dependencies: ["App"])
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/OpenKitten/Meow.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/GraphQLSwift/GraphQL.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/GraphQLSwift/Graphiti.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/GraphQLSwift/Graphiti.git", majorVersion: 0, minor: 3)
     ],
     exclude: [
         "Config",
         "Database",
         "Localization",
         "Public",
-        "Resources",
+        "Resources"
     ]
 )
-
